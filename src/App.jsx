61fb24ae4970ac1426d343d16330943bf7b17894
Renan Santos <renansantos7@proton.me>
2024-02-21 13:22:02 -0300

    Aplicado Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { RouterProvider } from "react-router-dom";
+import { RBankProvider } from "./contexts/RBankContext";
 import router from "./router";
 
 export default function App() {
   return (
-    <RouterProvider router={router} />
+    <RBankProvider>
+      <RouterProvider router={router} />
+    </RBankProvider>
   )
 }
